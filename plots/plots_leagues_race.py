72f95ec238ee1dcf08f0bf425bd6d3a6e5cb7ad3
Andreas Calleja <andreascalleja@gmail.com>
2023-04-15 19:43:13 +0200

    Trying video options

--- plots/plots_leagues_race.py
+++ plots/plots_leagues_race.py
@@ -3,6 +3,7 @@ import urllib.request
 from PIL import Image
 from moviepy.editor import *
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+import subprocess
 
 from constants import LEAGUES
 
@@ -23,46 +24,50 @@ def get_video(df, competition_name, lge, year):
 
 
 def freeze_video(lge):
-    video = (
-        VideoFileClip(f"videos/{lge}_clubs.mp4")
-        .fx(vfx.freeze, t="end", freeze_duration=3)
-        .fx(vfx.multiply_speed, 0.5)
+    subprocess.call(
+        'ffmpeg -i %s -vf tpad=stop_mode=clone:stop_duration=2, -vf "setpts=1.75*PTS", %s'
+        % (f"videos/{lge}_clubs.mp4", f"videos/{lge}_clubs_long.mp4"),
+        shell=True,
     )
 
-    if lge != "combined":
-        league_id = LEAGUES[lge].get("fotmob_id")
-        url = "https://images.fotmob.com/image_resources/logo/leaguelogo/"
-        icon = Image.open(urllib.request.urlopen(f"{url}{league_id}.png"))
+    # video = VideoFileClip(f"videos/{lge}_clubs_long.mp4").fx(
+    #     vfx.freeze, t="end", freeze_duration=3
+    # )
 
-        logo = (
-            ImageClip(icon)
-            .with_duration(video.duration)
-            .resize(height=95)
-            .margin(right=8, top=8, opacity=0)
-            .with_position(("right", "top"))
-        )
+    # if lge != "combined":
+    #     league_id = LEAGUES[lge].get("fotmob_id")
+    #     url = "https://images.fotmob.com/image_resources/logo/leaguelogo/"
+    #     icon = Image.open(urllib.request.urlopen(f"{url}{league_id}.png"))
 
-    footer_one = (
-        TextClip("Stats from fbref.com", font_size=25, color="black")
-        .with_position((334, video.h - 75))
-        .with_duration(video.duration)
-        .with_start(0)
-    )
+    #     logo = (
+    #         ImageClip(icon)
+    #         .with_duration(video.duration)
+    #         .resize(height=95)
+    #         .margin(right=8, top=8, opacity=0)
+    #         .with_position(("right", "top"))
+    #     )
 
-    footer_two = (
-        TextClip(
-            "Data Viz by Andreas Calleja @andreascalleja", font_size=25, color="black"
-        )
-        .with_position((330, video.h - 40))
-        .with_duration(video.duration)
-        .with_start(0)
-    )
+    # footer_one = (
+    #     TextClip("Stats from fbref.com", font_size=25, color="black")
+    #     .with_position((334, video.h - 75))
+    #     .with_duration(video.duration)
+    #     .with_start(0)
+    # )
 
-    final = CompositeVideoClip([video, logo or None, footer_one, footer_two])
-    final.write_videofile(f"videos/{lge}_clubs_draft.mp4", codec="libx264")
-    ffmpeg_extract_subclip(
-        f"videos/{lge}_clubs_draft.mp4",
-        0,
-        (final.duration - 3),
-        f"videos/final/{lge}_clubs.mp4",
-    )
+    # footer_two = (
+    #     TextClip(
+    #         "Data Viz by Andreas Calleja @andreascalleja", font_size=25, color="black"
+    #     )
+    #     .with_position((330, video.h - 40))
+    #     .with_duration(video.duration)
+    #     .with_start(0)
+    # )
+
+    # final = CompositeVideoClip([video, logo or None, footer_one, footer_two])
+    # final.write_videofile(f"videos/{lge}_clubs_draft.mp4", codec="libx264")
+    # ffmpeg_extract_subclip(
+    #     f"videos/{lge}_clubs_draft.mp4",
+    #     0,
+    #     (final.duration - 3),
+    #     f"videos/final/{lge}_clubs.mp4",
+    # )
